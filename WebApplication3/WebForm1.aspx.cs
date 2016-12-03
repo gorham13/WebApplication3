3dea92a4eebb65c2e9eec20097c6198d9fd0c0fb
HP <HP@HP->
2016-12-03 19:46:09 +0200

    checkpoint

--- WebApplication3/WebForm1.aspx.cs
+++ WebApplication3/WebForm1.aspx.cs
@@ -26,5 +26,7 @@ namespace WebApplication3
                 }
             }
         }
+
+        
     }
 }
--- WebApplication3/scripts/google_maps.js
+++ WebApplication3/scripts/google_maps.js
@@ -1,31 +1,17 @@
 ﻿        function initialize() {
-            var latlng = new google.maps.LatLng(-34.397, 150.644);
+            var latlng = new google.maps.LatLng(50.6198579, 26.2459409);
             var options =
             {
                 zoom: 15,
-                center: new google.maps.LatLng(50.6083811, 26.2580379),
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                mapTypeControl: true,
-                mapTypeControlOptions:
-                {
-                    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-                    poistion: google.maps.ControlPosition.TOP_RIGHT,
-                    mapTypeIds: [google.maps.MapTypeId.ROADMAP,
-                      google.maps.MapTypeId.TERRAIN,
-                      google.maps.MapTypeId.HYBRID,
-                      google.maps.MapTypeId.SATELLITE]
-                },
-                navigationControl: true,
-                navigationControlOptions:
-                {
-                    style: google.maps.NavigationControlStyle.ZOOM_PAN
-                },
-                scaleControl: true,
-                disableDoubleClickZoom: true,
-                draggable: false,
-                streetViewControl: true,
-                draggableCursor: 'move'
+                center: latlng
             };
-            var map = new google.maps.Map(document.getElementById("map"), options);
+            var map = new google.maps.Map(document.getElementById('map'), options);
+
+            var marker = new google.maps.Marker({
+                position: latlng,
+                map: map,
+                title: 'Театральна площа'
+            })
+            marker.setIcon("http://maps.google.com/mapfiles/ms/icons/red-dot.png")
         }
 window.onload = initialize;
